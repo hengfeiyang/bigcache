3406b97f3913f408ef015a48f67c67dc01d765ce
yanghengfei <hengfei.yang@gmail.com>
2022-04-14 12:12:42 +0800

    add command line for test

--- cache_v5.go
+++ cache_v5.go
@@ -1,1 +1,160 @@
 package bigcache
+
+import (
+	"bytes"
+	"encoding/binary"
+	"math"
+	"sync"
+	"time"
+)
+
+// cacheV5 use shards map and bytes storage
+type cacheV5 struct {
+	shards  []*cacheV5ByteStorage
+	bitMask uint64
+}
+
+type cacheV5ByteStorage struct {
+	data    map[uint64]int
+	storage []byte
+	lock    sync.RWMutex
+	head    int
+	tail    int
+	length  int
+}
+
+type cacheV5Item struct {
+	Expires     uint64
+	KeyLength   uint16
+	ValueLength uint32
+	Key         []byte
+	Value       []byte
+}
+
+func NewCacheV5(capacity, maxEntrySize, shards int) Cacher {
+	if shards == 0 {
+		shards = 128
+	}
+	if shards&(shards-1) != 0 {
+		shards = int(math.Pow(2, math.Ceil(math.Log2(float64(shards)))))
+	}
+	t := &cacheV5{
+		shards:  make([]*cacheV5ByteStorage, shards),
+		bitMask: uint64(shards - 1),
+	}
+	for i := 0; i < shards; i++ {
+		t.shards[i] = NewCacheV5ByteStorage(capacity/shards, maxEntrySize/shards)
+	}
+	return t
+}
+
+func (t *cacheV5) Set(key string, value []byte, ttl time.Duration) error {
+	keyHash := t.getHashKey(key)
+	shardKey := int(keyHash & t.bitMask)
+	return t.shards[shardKey].Set(keyHash, value, ttl)
+}
+
+func (t *cacheV5) Get(key string) ([]byte, error) {
+	keyHash := t.getHashKey(key)
+	shardKey := int(keyHash & t.bitMask)
+	return t.shards[shardKey].Get(keyHash)
+}
+
+func (t *cacheV5) TTL(key string) (time.Duration, error) {
+	keyHash := t.getHashKey(key)
+	shardKey := int(keyHash & t.bitMask)
+	return t.shards[shardKey].TTL(keyHash)
+}
+
+func (t *cacheV5) Delete(key string) {
+	keyHash := t.getHashKey(key)
+	shardKey := int(keyHash & t.bitMask)
+	t.shards[shardKey].Delete(keyHash)
+}
+
+func (t *cacheV5) Len() int {
+	i := 0
+	for k := range t.shards {
+		i += t.shards[k].Len()
+	}
+	return i
+}
+
+func (t *cacheV5) getHashKey(key string) uint64 {
+	return NewDefaultHasher().Sum64(key)
+
+}
+
+func NewCacheV5ByteStorage(capacity, maxEntrySize int) *cacheV5ByteStorage {
+	return &cacheV5ByteStorage{
+		data:    make(map[uint64]int, capacity),
+		storage: make([]byte, maxEntrySize),
+	}
+}
+
+const headerLength = 8 + 4 // timestamp:8 + valueLength:4
+
+func (t *cacheV5ByteStorage) Set(keyHash uint64, value []byte, ttl time.Duration) error {
+	expires := time.Time{}
+	if ttl > 0 {
+		expires = time.Now().Add(ttl)
+	}
+	buf := bytes.NewBuffer(nil)
+	needSize := headerLength + len(value)
+	t.lock.Lock()
+	pos := t.tail
+	if cap(t.storage)-len(t.storage) < needSize {
+		// extend storage
+		newStorage := make([]byte, cap(t.storage)+needSize*2)
+		copy(newStorage, t.storage)
+		t.storage = newStorage
+	}
+	binary.Write(buf, binary.LittleEndian, expires.Unix())     // int64 -> 8
+	binary.Write(buf, binary.LittleEndian, uint32(len(value))) // uint32 -> 4
+	copy(t.storage[t.tail:], buf.Bytes())
+	copy(t.storage[t.tail+headerLength:], value) // body
+	t.data[keyHash] = pos
+	t.tail += headerLength + len(value)
+	t.length++
+	t.lock.Unlock()
+	buf.Reset()
+	return nil
+}
+
+func (t *cacheV5ByteStorage) Get(keyHash uint64) ([]byte, error) {
+	t.lock.RLock()
+	pos, ok := t.data[keyHash]
+	if !ok {
+		t.lock.RUnlock()
+		return nil, ErrNotExist
+	}
+	header := t.storage[pos : pos+headerLength]
+	t.lock.RUnlock()
+	buf := bytes.NewBuffer(header)
+	var expires int64
+	var keyLength uint16
+	var bodyLength uint32
+	binary.Read(buf, binary.LittleEndian, &expires)
+	binary.Read(buf, binary.LittleEndian, &keyLength)
+	binary.Read(buf, binary.LittleEndian, &bodyLength)
+	buf.Reset()
+	if expires > 0 && expires < time.Now().Unix() {
+		return nil, ErrNotExist // expired
+	}
+	t.lock.RLock()
+	buf.Write(t.storage[pos+int(headerLength)+int(keyLength) : pos+int(headerLength)+int(keyLength)+int(bodyLength)])
+	t.lock.RUnlock()
+	return buf.Bytes(), nil
+}
+
+func (t *cacheV5ByteStorage) TTL(keyHash uint64) (time.Duration, error) {
+	return 0, nil
+}
+
+func (t *cacheV5ByteStorage) Delete(keyHash uint64) {
+
+}
+
+func (t *cacheV5ByteStorage) Len() int {
+	return t.length
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,180 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hengfeiyang/bigcache"
 )
 
+const defaultDapacity = 10000
+const defaultShards = 32
+const defaultMaxEntrySize = 1024 * 1024 // 1M
+
+var ErrorCommandNotFound = errors.New("command not found")
+
+const help = `Command not found:
+DB n                   select db of version, support: 1,2,3,4,5, default is 4
+SET key value [TTL]    set a value by the key and optional ttl, unit is: time.Second
+GET key                get a value by the key
+TTL key                get the remain ttl by the key
+DEL key                delete the key
+LEN                    get total entries of current db
+`
+
+var dbs []bigcache.Cacher
+var currentDB = 4
+
+type command struct {
+	Command string
+	Key     string
+	Value   string
+	TTL     time.Duration
+	DB      int
+}
+
 func main() {
-	cache := bigcache.NewCacheV4(1024*1024, 128)
-	for i := 0; i < 1024*1024; i++ {
-		cache.Set(key(i), value(i), 0)
-	}
-	for i := 0; ; i++ {
-		cache.Delete(key(i))
-		cache.Set(key(1024*1024+i), value(i), 0)
-		time.Sleep(10 * time.Millisecond)
+	dbs = make([]bigcache.Cacher, 10)
+	dbs[1] = bigcache.NewCacheV1(defaultDapacity)
+	dbs[2] = bigcache.NewCacheV2(defaultDapacity)
+	dbs[3] = bigcache.NewCacheV3(defaultDapacity)
+	dbs[4] = bigcache.NewCacheV4(defaultDapacity, defaultShards)
+	dbs[5] = bigcache.NewCacheV5(defaultDapacity, defaultMaxEntrySize, defaultShards)
+
+	fmt.Println("bigcache cli v0.0.1")
+	reader := bufio.NewReader(os.Stdin)
+	var output string
+	for {
+		fmt.Printf("db[%d]> ", currentDB)
+		s, _, _ := reader.ReadLine()
+		cmd, err := parseCommand(string(s))
+		if err != nil {
+			if err == ErrorCommandNotFound {
+				fmt.Print(help)
+			} else {
+				fmt.Println(err)
+			}
+			continue
+		}
+
+		switch cmd.Command {
+		case "DB":
+			output, err = cmd.db()
+		case "SET":
+			output, err = cmd.set()
+		case "GET":
+			output, err = cmd.get()
+		case "TTL":
+			output, err = cmd.ttl()
+		case "DEL":
+			output, err = cmd.delete()
+		case "LEN":
+			output, err = cmd.len()
+		}
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			if len(output) > 0 {
+				fmt.Println(output)
+			}
+		}
 	}
 }
 
-func key(i int) string {
-	return fmt.Sprintf("key-%d", i)
+func parseCommand(s string) (*command, error) {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return nil, ErrorCommandNotFound
+	}
+
+	column := strings.Split(s, " ")
+	cmd := new(command)
+	cmd.Command = strings.ToUpper(column[0])
+	switch cmd.Command {
+	case "DB":
+		if len(column) < 2 {
+			return nil, fmt.Errorf("DB n")
+		}
+		cmd.DB, _ = strconv.Atoi(column[1])
+		if cmd.DB > 5 {
+			return nil, fmt.Errorf("db range is [0-5]")
+		}
+	case "SET":
+		if len(column) < 3 {
+			return nil, fmt.Errorf("SET key value [TTL], unit is: time.Second")
+		}
+		cmd.Key = column[1]
+		cmd.Value = column[2]
+		if len(column) == 4 {
+			ttl, _ := strconv.Atoi(column[3])
+			cmd.TTL = time.Duration(ttl) * time.Second
+		}
+	case "GET":
+		if len(column) < 2 {
+			return nil, fmt.Errorf("GET key")
+		}
+		cmd.Key = column[1]
+	case "TTL":
+		if len(column) < 2 {
+			return nil, fmt.Errorf("TTL key")
+		}
+		cmd.Key = column[1]
+	case "DEL":
+		if len(column) < 2 {
+			return nil, fmt.Errorf("DEL key")
+		}
+		cmd.Key = column[1]
+	case "LEN":
+		// noop
+	default:
+		return nil, ErrorCommandNotFound
+	}
+
+	return cmd, nil
 }
 
-func value(i int) []byte {
-	b := bytes.NewBuffer(nil)
-	b.WriteString("value-")
-	b.Write([]byte(strconv.Itoa(i)))
-	return b.Bytes()
+func (t *command) db() (string, error) {
+	switch t.DB {
+	case 1, 2, 3, 4, 5:
+		currentDB = t.DB
+	default:
+		currentDB = 4
+	}
+	return "", nil
+}
+
+func (t *command) set() (string, error) {
+	err := dbs[currentDB].Set(t.Key, []byte(t.Value), t.TTL)
+	return "OK", err
+}
+
+func (t *command) get() (string, error) {
+	val, err := dbs[currentDB].Get(t.Key)
+	return string(val), err
+}
+
+func (t *command) ttl() (string, error) {
+	ttl, err := dbs[currentDB].TTL(t.Key)
+	if err != nil {
+		return "", err
+	}
+	if ttl > 0 {
+		ttl /= time.Second
+	}
+	return fmt.Sprintf("(integer) %d", ttl), nil
+}
+
+func (t *command) delete() (string, error) {
+	dbs[currentDB].Delete(t.Key)
+	return fmt.Sprintf("(integer) %d", 1), nil
+}
+
+func (t *command) len() (string, error) {
+	n := dbs[currentDB].Len()
+	return strconv.Itoa(n), nil
 }
